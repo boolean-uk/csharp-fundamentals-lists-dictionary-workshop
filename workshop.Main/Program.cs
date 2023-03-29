4508055c61ca24494a581c3da23481e2746f4e3b
uerbzr <nigel.sibbert@gmail.com>
2023-03-29 12:57:40 +0100

    update

--- workshop.Main/Program.cs
+++ workshop.Main/Program.cs
@@ -1,5 +1,11 @@
-﻿namespace workshop.Main
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace workshop.Main
 {
+    public class Person
+    {
+        public string Name { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -33,15 +39,86 @@
 
 
             //3
-            var passengerList = new List<string>()
+            var JavaClass = new List<string>()
             {
-                "Nigel",
-                "Bob",
-                "Linda",
-                "Emma"
+                "Java Nigel",
+                "Java Bob",
+                "Java Linda",
+                "Java Emma"
             };
 
-            
+            var CSharpClass = new List<string>()
+            {
+                "C# Emily",
+                "C# Nadine",
+                "C# Marie",
+                "C# Frederic",
+                "C# Mo"
+            };
+
+            //var BothClasses = new List<string>();
+            //BothClasses.AddRange(JavaClass);
+            //BothClasses.AddRange(CSharpClass);
+
+
+
+            var UnassignedStudents = new List<string>()
+            {
+                "Footballer Ronaldo",
+                "Footballer Maradona"
+            };
+
+            List<string> AllStudents = new List<string>();
+            AllStudents.AddRange(JavaClass);
+            AllStudents.AddRange(CSharpClass);
+            AllStudents.AddRange(UnassignedStudents);
+
+            JavaClass.Add("JAVA Barry");
+
+            //var AllStudents = JavaClass.Concat(CSharpClass).Concat(UnassignedStudents).ToList();
+
+
+
+            List<Person> people1 = new List<Person>()
+            {
+                new Person() { Name = "bob"},
+                new Person() { Name = "bib"},
+                new Person() { Name = "bab"},
+                new Person() { Name = "bub"}
+            };
+
+            List<Person> people2 = new List<Person>()
+            {
+                new Person() { Name = "1bob"},
+                new Person() { Name = "1bib"},
+                new Person() { Name = "1bab"},
+                new Person() { Name = "1bub"}
+            };
+
+
+
+            //var both = Persons.Concat(Persons2).ToList<Person>();
+            List<Person> both = new List<Person>();
+            //both.AddRange(Persons);
+            //both.AddRange(Persons2);
+
+            people1.ForEach(p => { 
+                both.Add(new Person() { Name = p.Name });
+            });
+            people2.ForEach(p => {
+                both.Add(new Person() { Name = p.Name });
+            });
+
+            people2.ForEach(x => {
+                x.Name = $"v2.2_{x.Name}";
+            });
+
+
+
+
+
+
+
             //4
             var ages = new List<int>();
             ages.Add(2);
